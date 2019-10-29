a9be54f82514d02f3f76387e29104a11f9e182d9
Iago Tito <iago.oliveira@ccc.ufcg.edu.br>
2019-10-29 09:19:09 -0300

    Initial commit with spring project initializated

---
+++ ajude/src/test/java/psoft/proj/backend/ajude/AjudeApplicationTests.java
@@ -0,0 +1,13 @@
+package psoft.proj.backend.ajude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
