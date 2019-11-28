5a101e3580d450e58daba13811ab958f244c99b1
Diego Ribeiro <diego.almeida@ccc.ufcg.edu.br>
2019-11-28 05:11:46 -0300

    Config Swagger

--- src/main/java/psoft/proj/backend/ajude/users/services/EmailService.java
+++ src/main/java/psoft/proj/backend/ajude/users/services/EmailService.java
@@ -21,7 +21,7 @@ public class EmailService {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(userEmail);
         msg.setSubject("Confirmação de cadastro no AJuDE");
-        msg.setText("Bem-vindo(a) ao AJuDE!\n\nFaça seu login em psoft-ajude-o-grupo-13.surge.sh ");
+        msg.setText("Bem-vindx ao AJuDE! Muito obrigado por se juntar a nós!!\n\nAgora você já pode participar da nossa plataforma.\nFaça seu login em psoft-ajude-o-grupo-13.surge.sh");
         try {
             mailSender.send(msg);
         } catch (Exception e){
